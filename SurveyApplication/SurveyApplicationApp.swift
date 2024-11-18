f45e943f8b5365233c939afdde2602f6dd131be8
Abdulsamet Gocmen <hellosamet09@icloud.com>
2024-11-18 22:56:01 +0300

    Initial Commit

---
+++ SurveyApplication/SurveyApplicationApp.swift
@@ -0,0 +1,32 @@
+//
+//  SurveyApplicationApp.swift
+//  SurveyApplication
+//
+//  Created by Abdulsamet Göçmen on 18.11.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SurveyApplicationApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SurveyApplicationTests/SurveyApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApplicationTests.swift
+//  SurveyApplicationTests
+//
+//  Created by Abdulsamet Göçmen on 18.11.2024.
+//
+
+import Testing
+@testable import SurveyApplication
+
+struct SurveyApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
